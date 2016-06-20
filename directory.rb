bb0b690935487a44b6c3d7e6d1010189c5e13d90
Tommy Williams <tom@teow.co.uk>
2016-06-20 12:48:37 +0100

    Added list of students

---
+++ directory.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
