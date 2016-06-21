a076b4ba94a0f7a3f9a5ab87c3f805d78ec14b7b
Tommy Williams <tom@teow.co.uk>
2016-06-21 15:08:24 +0100

    Output sorted by cohort

--- directory.rb
+++ directory.rb
@@ -7,14 +7,41 @@ def input_students
   #Get the first names
   name = gets.chomp
   # While the array is not empty, repeat this code
+  puts "Is #{name} spelt correctly? y/n"
+  spell = gets.chomp.downcase
+  until spell == "n" || spell == "y"
+    puts "You didn't enter 'y' or 'n', please re-enter:"
+    spell = gets.chomp.downcase
+  end
+  while spell == "n"
+    puts "Please re-enter the name spelt correctly:"
+    name = gets.chomp
+    puts "Is #{name} spelt correctly? y/n"
+    spell = gets.chomp.downcase
+    until spell == "n" || spell == "y"
+      puts "You didn't enter 'y' or 'n', please re-enter:"
+      spell = gets.chomp.downcase
+    end
+  end
   while !name.empty? do
     # Add the student hash to the array
-    puts "Please enter #{name}'s cohort"
+    puts "Please enter #{name}'s cohort in the format 'monthYY'"
     cohort = gets.chomp
     while cohort.empty? do
       puts "You must enter #{name}'s cohort"
-      cohort = gets.chomp.to_sym
+      cohort = gets.chomp
     end
+    $cohort_months = [ "april15", "april16", "august14",
+                      "december14", "february15", "february16",
+                      "january16", "july15", "june15", "may14",
+                      "november15", "october14", "october15",
+                      "september14", "september15", "july16",
+                      "may16"]
+    until $cohort_months.include?(cohort)
+      puts "You did not enter a valid cohort e.g. july16"
+      cohort = gets.chomp
+    end
+    cohort.to_sym
     hobbies = []
     puts "Please enter #{name}'s hobbies, one by one followed by return."
     hobby_each = gets.chomp
@@ -41,12 +68,33 @@ def print_header
   puts "The students of Villains Academy".center(50)
   puts "-"*50
 end
-
+=begin
 def print_name(students)
   i = 0
   while (i < students.length)
-      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort, born in #{country_birth}, whose hobbies include: #{students[i][:hobbies].join(", ")})"
+      puts "#{i+1}.\tName: #{students[i][:name]}\n\tCohort: #{students[i][:cohort]}\n\tBorn in: #{students[i][:country_birth]}\n\tHobbies: #{students[i][:hobbies].join(", ")}"
       i += 1
+  end
+end
+
+def print_cohorts(students)
+  i = 0
+  while (i < students.length)
+      puts "#{i+1}.\tName: #{students[i][:name]}\n\tCohort: #{students[i][:cohort]}\n\tBorn in: #{students[i][:country_birth]}\n\tHobbies: #{students[i][:hobbies].join(", ")}"
+      i += 1
+  end
+end
+=end
+
+def print_cohorts(students)
+  cohort_sorted = students.group_by { |e| e[:cohort] }
+  cohort_sorted.each do |k,v|
+    puts "-" * 50
+    puts ""
+    print "Cohort: #{k}\t"
+    v.each do |student|
+      puts "\n\tName: #{student[:name]}\n\tBorn in: #{student[:country_birth]}\n\tHobbies: #{student[:hobbies].join(", ")}"
+    end
   end
 end
 
@@ -66,7 +114,8 @@ end
 
 students = input_students
 print_header
-print_name($students)
+#print_name($students)
+print_cohorts($students)
 print_footer($students)
 
 =begin
