dfc8cb009299622a8fed5bc8d8c9db1ccee88da9
Tommy Williams <tom@teow.co.uk>
2016-06-21 10:25:49 +0100

    Collecting more details

--- directory.rb
+++ directory.rb
@@ -1,26 +1,56 @@
+$students = []
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit retrun twice"
+  puts "To finish, just hit retrun twice on name"
   #Create empty array
-  students = []
+  $students = []
   #Get the first names
   name = gets.chomp
   # While the array is not empty, repeat this code
   while !name.empty? do
     # Add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Please enter #{name}'s cohort"
+    cohort = gets.chomp
+    while cohort.empty? do
+      puts "You must enter #{name}'s cohort"
+      cohort = gets.chomp.to_sym
+    end
+    hobbies = []
+    puts "Please enter #{name}'s hobbies, one by one followed by return."
+    hobby_each = gets.chomp
+    while !hobby_each.empty? do
+      hobbies << hobby_each
+      hobby_each = gets.chomp
+    end
+    puts "Please enter #{name}'s country of birth"
+    country_birth = gets.chomp
+    while country_birth.empty? do
+      puts "You must enter #{name}'s country of birth"
+      country_birth = gets.chomp
+    end
+    $students << {name: name, cohort: cohort, hobbies: hobbies, country_birth: country_birth}
+    puts "Now we have #{$students.count} students"
     # Get another name from the user
+    puts "Please enter the name of the next student"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "-"*50
+  puts "The students of Villains Academy".center(50)
+  puts "-"*50
 end
 
+def print_name(students)
+  i = 0
+  while (i < students.length)
+      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort, born in #{country_birth}, whose hobbies include: #{students[i][:hobbies].join(", ")})"
+      i += 1
+  end
+end
+
+=begin
 def print(students)
   students.each_with_index do |student, index|
     if student[:name].length < 12
@@ -28,6 +58,7 @@ def print(students)
     end
   end
 end
+=end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
@@ -35,8 +66,8 @@ end
 
 students = input_students
 print_header
-print(students)
-print_footer(students)
+print_name($students)
+print_footer($students)
 
 =begin
 print_header
