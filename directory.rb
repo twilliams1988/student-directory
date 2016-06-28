fe7c33499dee75b8ce5c056e2952f54bd583d6fa
Tommy Williams <tom@teow.co.uk>
2016-06-28 15:46:27 +0100

    Ability to save added

--- directory.rb
+++ directory.rb
@@ -132,6 +132,7 @@ def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -147,6 +148,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -154,6 +157,17 @@ def process(selection)
   end
 end
 
+def save_students
+  # open file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    students_data = [student[:name], student[:cohort], student[:hobbies], student[:country_birth]]
+    csv_line = students_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 def interactive_menu
   @students = []
